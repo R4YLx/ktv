501fa67e8f0949f6531ab903f59c39cca5c53a76
Johanna <johannakoreo@gmail.com>
2022-03-14 17:58:58 +0100

    added some outcommented code for using channels in socket.io

--- bin/server.js
+++ bin/server.js
@@ -6,11 +6,24 @@
 
 require('dotenv').config();
 
+	/*Lägga till stöd för kanaler i socket.io. +npm install express ejs. allt detta för att kunna använda CHAT ROOMS/ GAME ROOMS
+	//const express = require('express');
+	//const app = express(); 
+	//const server = require('http).Server(app); //skcikar in application servern här. //ger oss en server som kan connecta med server.io vilket gör att vi kan använda de här io-grejorna i vår kod.
+	//const io = require('socket.io')(server);
+
+	app.set('views', './views'); //create a views-folder for this path to function
+	app.set('view engine', 'ejs');
+	appuse(express.static('public)); //here goes all code for the javascript for the client side. in this folder
+	app.use(express.urlencoded({ extended: true }));  //gör att vi kan använbda url-encoded parameters istllet för en body för en form
+	*/
+
 const app = require('../app');
 const debug = require('debug')('game:server');
 const http = require('http');
 const socketio = require('socket.io');
 const socket_controller = require('../controllers/socket_controller');
+
 
 /**
  * Get port from environment and store in Express.
