d2dcad5180b4647751be6cdfca9817321cf7acca
Raymond Lam <raymond.m.lam@gmail.com>
2022-03-14 15:31:29 +0100

    Added stödord

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -1,3 +1,9 @@
 /**
  * Socket Controller
  */
+
+// Create a function for handling joined user
+const handleUserJoined = function () {};
+
+// Put available players into one gameroom
+const assignUserToRoom = function () {};
