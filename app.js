98c11a739742b16eb6663021b2d34daca821fdf8
jo-oo <johannakoreo@gmail.com>
2022-03-15 17:08:18 +0100

    removed routes

--- app.js
+++ app.js
@@ -11,9 +11,6 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-// routes
-app.use(require("./routes"));
-
 // serve static files from public folder
 app.use(express.static("public"));
 
