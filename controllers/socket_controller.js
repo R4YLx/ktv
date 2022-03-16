a187984ab36544404ab3530f4f0267ac20c10031
Raymond Lam <raymond.m.lam@gmail.com>
2022-03-16 14:04:35 +0100

    fixed issue with submit. Issue was in html. Added functions for submit

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -5,7 +5,7 @@
 const debug = require('debug')('ktv:socket_controller'); //läser in debug
 
 // Socket.io server instance
-let io = null; 
+let io = null;
 
 // List of users
 const users = {};
@@ -13,60 +13,53 @@ const users = {};
 //List of rooms
 const rooms = [{}];
 
-    
 // Create a function for handling joined user
 const handleUserJoined = function (username, room_id, callback) {
-    debug(`User ${username} with socket id ${this.id} wants to join room '${room_id}'`);
+	debug(
+		`User ${username} with socket id ${this.id} wants to join room '${room_id}'`
+	);
 
-    // Join room
-    this.join(room_id); 
+	// Join room
+	this.join(room_id);
 
-    // Find room 
-    const room = rooms.find(gameroom => gameroom.id === room_id)
+	// Find room
+	const room = rooms.find(gameroom => gameroom.id === room_id);
 
-    // Add socket to room's users-object
-    room.users[this.id] = username;
-
+	// Add socket to room's users-object
+	// room.users[this.id] = username;
 };
 
 // Put available players into one gameroom
 const assignUserToRoom = function () {
-
-    socket.emit('Welcome to the pixel game', 'WELCOME TO THE PIXEL GAME!');
-
+	socket.emit('Welcome to the pixel game', 'WELCOME TO THE PIXEL GAME!');
 };
-
 
 // // Handle user disconnect
 // socket.on('disconnect', function() {
 // debug(`Player ${this.id} disconnected`);
-  
+
 // //broadcast to everyone else that the user disconnected (vi kan annars strunta i att broadcasta detta och bara skicka ut felmeddelande till den andra spelaren/alternativt att spelet bara avbryts och den får ett "du-vann-meddelande!" bara )
 // this.broadcast.emit('user:disconnected');
-// }); 
+// });
 
+//det finns en socket till varje ny anslutning av en person till spelet
+module.exports = function (socket, _io) {
+	debug('A new client has connected', socket.id);
 
- //det finns en socket till varje ny anslutning av en person till spelet
-module.exports = function(socket, _io) {
-	  debug('A new client has connected', socket.id);
-
-	  // Handle user joined
-	  socket.on('user:joined', handleUserJoined); 
+	// Handle user joined
+	socket.on('user:joined', handleUserJoined);
 };
 
-
 // 6. Randomize virusPosition
-
 
 // 7. Randomize DelayVirusDisplay - SET TIMEOUT, slumpar en delay-tid innan Virus-position sätts ut
 
 //<h2 id="VirusHere"></h2> //put in html
 //put in JS chat and not here (?????)
-  /*
-  const myTimeout = setTimeout(delayVirusDisplay, 5000); //sets the timer to 5 seconds
-
-  function delayVirusDisplay() {
-    document.getElementById("VirusHere").innerHTML = "Virus comes after you now after 5 seconds!"
-  }
-  */
-  //vi kan annars göra en onclick på viruset: <button onclick="setTimeout(delayVirusDisplay, 3000);">Click this virus!</button>
+/*
+   const myTimeout = setTimeout(delayVirusDisplay, 5000); //sets the timer to 5 seconds
+   function delayVirusDisplay() {
+	 document.getElementById("VirusHere").innerHTML = "Virus comes after you now after 5 seconds!"
+   }
+   */
+//vi kan annars göra en onclick på viruset: <button onclick="setTimeout(delayVirusDisplay, 3000);">Click this virus!</button>
