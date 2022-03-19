07e2127cc59e5c8c862010c718098666b1845790
Raymond Lam <raymond.m.lam@gmail.com>
2022-03-19 11:08:31 +0100

    randomize virus position and delay on click on server side working

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -6,10 +6,14 @@ const debug = require('debug')('ktv:socket_controller');
 let io = null;
 
 let players = {};
-let availableRoom = 1;
-let games = [];
 
-const getVirus = () => {};
+const getRandomPosition = () => {
+	return Math.floor(Math.random() * 21);
+};
+
+const getRandomDelay = () => {
+	return Math.floor(Math.random() * 5000);
+};
 
 // Function for handling connecting players
 const handlePlayerJoined = function (username, callback) {
@@ -17,14 +21,26 @@ const handlePlayerJoined = function (username, callback) {
 
 	players[this.id] = username;
 
+	this.emit('player:connected', username);
+
 	callback({ success: true });
 };
 
-module.exports = function (socket) {
-	io = this;
+const handleClick = function (target) {
+	debug('Someone clicked on the virus', target);
+
+	setTimeout(() => {
+		this.emit('virus:position', getRandomPosition(), getRandomPosition());
+	}, getRandomDelay());
+};
+
+module.exports = function (socket, _io) {
+	io = _io;
 
 	debug('On start of app: a new client has connected', socket.id);
 
 	// handle player connect
 	socket.on('playerJoined', handlePlayerJoined);
+
+	socket.on('virus:click', handleClick);
 };
--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -5,23 +5,26 @@ const startEl = document.querySelector('#start');
 const waitingEl = document.querySelector('#waiting');
 const gameWrapperEl = document.querySelector('#gameWrapper');
 const gameAreaEl = document.querySelector('#gameArea');
-const noticeEl = document.querySelector('#notice');
+const virusEl = document.querySelector('#virus');
 const usernameFormEl = document.querySelector('#usernameForm');
 const playerUsernameEl = document.querySelector('#playerUsername');
 const playerScoreEl = document.querySelector('#playerScore');
-const opponentScoreEl = document.querySelector('#playerScore');
-const playAgainButtonEl = document.querySelector('#playAgainButton');
-const scoreboardEl = document.querySelector('#scoreboard');
 const currentRoundEl = document.querySelector('#currentRound');
 const showRoundsEl = document.querySelector('#showRounds');
-const messageEl = document.querySelector('#message');
 
 let username = null;
 let score = 0;
-//let showRounds = 0;
+let showRounds = 0;
 let currentRound = 0;
-let virus = './assets/icons/virus.png';
-let continueGame = true;
+
+// Get time and calculate
+let interval;
+let startTime;
+let elapsedTime;
+let stopTime;
+let playerTime;
+let sum = 0;
+let timeSum = [];
 
 /*//////
 //  Functions 
@@ -42,111 +45,44 @@ const setInnerText = (element, value) => {
 	element.innerText = value;
 };
 
+// set innerHTML value
 const setInnerHTML = (element, value) => {
 	element.innerHTML = value;
 };
 
-const getRandomNumber = value => {
-	return Math.floor(Math.random() * value) + 1;
+// Saras Timer-function: Start timer when virus is on display
+let startTimer = () => {
+	let startTime = Date.now();
+	interval = setInterval(function () {
+		let elapsedTime = Date.now() - startTime;
+
+		document.querySelector('#playerOneTime').innerHTML = (
+			elapsedTime / 1000
+		).toFixed(3); //(3)- is nr of decimals
+	}, 100);
 };
 
-const showLightbox = () => {
-	if (noticeEl.style.display === 'grid') {
-		noticeEl.style.display = 'none';
-		noticeEl.classList.remove('showLightbox');
-	} else {
-		noticeEl.style.display = 'grid';
-		noticeEl.classList.add('showLightbox');
-	}
+const setVirusPosition = (col, row) => {
+	virusEl.style.gridColumn = `${col} / span 1`;
+	virusEl.style.gridRow = `${row} / span 1`;
+
+	displayElement(virusEl);
 };
-
-const lightboxInfo = (textInBox, buttonId, textInButton) => {
-	let infobox = document.createElement('div');
-	infobox.className = 'infobox';
-	noticeEl.appendChild(infobox);
-
-	let infoboxEl = infobox;
-
-	let infoboxText = document.createElement('p');
-	infoboxText.className = 'lightboxText';
-	infoboxText.innerHTML = textInBox;
-	infoboxEl.appendChild(infoboxText);
-
-	let button = document.createElement('button');
-	button.id = buttonId;
-	button.className = 'btn-lg btn-danger';
-	button.innerHTML = textInButton;
-	infoboxEl.appendChild(button);
-};
-
-//Creates numbers between 1-26 and let´s that number be equal to the grid-position of the same div-box.
-const getGrid = () => {
-	for (let i = 1; i < 27; i++) {
-		let gridbox = document.createElement('div');
-		gridbox.id = i;
-		gridbox.className = 'gridbox' + i + ' ' + 'gridbox';
-		// gridbox.src = '';
-		gameAreaEl.appendChild(gridbox);
-	}
-};
-getGrid();
-
-//randomizes the grid-positions between 1-26. Puts the virus-image in that grid-div-box.
-const randomizedVirusPosition = () => {
-	let gridPosition = Math.floor(Math.random() * 26) + 1;
-	let position = document.getElementById(gridPosition);
-	let virus = document.createElement('img');
-	virus.id = 'virus';
-	virus.src = './assets/icons/virus.png';
-	position.appendChild(virus);
-};
-
-//DISPLAY VIRUS WITH RANDOM DELAY
-const showVirus = () => {
-	setTimeout(() => {
-		randomizedVirusPosition(); //calls the display-virus-function
-		//window.alert('Done waiting');
-	}, Math.floor(Math.random() * 5000)); //slumpar ut viruset mellan 0 och 5 sekunder
-};
-//add startTimer to showVirus-function
-showVirus();
 
 /*//////
 //  Events
 /////*/
 
 gameAreaEl.addEventListener('click', e => {
-	//prevents default page reload
-	e.preventDefault();
+	console.log(e.target);
+	socket.emit('virus:click');
 
 	// Click event for virus
-	if (e.target.tagName === 'IMG') {
+	if (e.target.id === 'virus') {
 		console.log('You killed the virus!');
-		score++; //Players score is updated
 		setInnerText(playerScoreEl, score);
 		setInnerText(currentRoundEl, score);
-	}
-
-	//sets game to equal 10 rounds
-	if (score == 10) {
-		messageEl.innerHTML = `<p>CONGRATULATIONS YOU WON!</p> 
-                <button type="playAgainButton">Play again</button>
-                `;
-		score = 0;
-	}
-
-	//Play again event
-	if (e.target.getAttribute('type') === 'playAgainButton') {
-		continueGame = false;
-		score = 0;
-		setInnerText(playerScoreEl, score);
-		setInnerText(currentRoundEl, score);
-		currentRoundEl.innerHTML = 0;
-		//currentRoundEl.innerHTML = ""; //nollställer Scoreboard
-		//showRoundsEl.innerHTML = ""; //nollställer Scoreboard
-		//playerScoreEl.innerHTML= ""; //nolställer Spelare 1:s poäng
-		//opponentScoreEl.innerHTML=""; //nolställer Spelare 2:s poäng
-		messageEl.innerHTML = ''; //nollställer "Grattis-meddelandet"
+	} else {
 	}
 });
 
@@ -156,7 +92,6 @@ usernameFormEl.addEventListener('submit', e => {
 
 	username = usernameFormEl.username.value;
 
-	// emit 'user:joined' event and when we get acknowledgement, THEN show chat
 	socket.emit('playerJoined', username, status => {
 		if (status.success) {
 			hideElement(startEl);
@@ -167,3 +102,9 @@ usernameFormEl.addEventListener('submit', e => {
 		}
 	});
 });
+
+/*//////
+//  Socket events
+/////*/
+
+socket.on('virus:position', setVirusPosition);
