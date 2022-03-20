eb4046733c8cdb5567e49c3f0b09ce64b8f199f0
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-20 16:11:23 +0100

    Added models with structure

---
+++ models/Model.js
@@ -0,0 +1,46 @@
+/*/////
+//  Modules
+/////*/
+
+
+// ROOM ID
+const roomId = () => {};
+
+
+
+
+
+// GET PLAYER BY ROOM ID
+const playerId = () => {};
+
+
+
+
+
+// OPPONENT ID
+const opponentId = () => {};
+
+
+
+
+// SCORE
+const score = () => {};
+
+
+
+
+
+// RESULT (WINNER)
+const winner = () => {};
+
+
+
+
+
+module.exports = {
+    roomId,
+    playerId,
+    opponentId,
+    score,
+    winner,
+};
