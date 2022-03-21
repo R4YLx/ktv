0a64c846013dbedfb097661e12b451fbdc1ed829
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-21 10:19:10 +0100

    added handleReactionTime

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -5,91 +5,89 @@
 const debug = require('debug')('ktv:socket_controller');
 let io = null;
 
-const { 
-	roomId, 
-	playerId, 
-	opponentId,
-	score, 
-	winner 
-} = require('../models/Model');
+// get functions from models
+const { roomId, playerId, opponentId,score, winner } = require('../models/Model');
 
-let players = {};
 const rooms = {};
-let maxRounds = 10;
-let rounds = 0;
+const thisGame = {};
 
+const players = {};
+const lobby = {};
+ 
 const getRandomPosition = () => {
 	return Math.floor(Math.random() * 21);
 };
-
+ 
 const getRandomDelay = () => {
 	return Math.floor(Math.random() * 5000);
 };
-
+ 
 const getVirus = () => {
 	let col = Math.floor(Math.random() * 21);
 	let row = Math.floor(Math.random() * 21);
 	let delay = Math.floor(Math.random() * 5000);
-
+ 
 	this.emit('virus:get', col, row, delay);
 };
-
+ 
 // Handle connecting players
 const handlePlayerJoined = function (username, callback) {
-	debug(`User ${username} with socket id ${this.id} has joined.`);
+	debug(`User ${username} with socket id ${this.id} has joined ${lobby}.`);
 	players[this.id] = username;
-
+ 
+	this.join(lobby);
+ 
 	this.emit('player:connected', username);
-
+ 
 	callback({ success: true });
 };
-
+ 
 // Handle disconnecting players
 const handleDisconnect = function () {
 	debug(`Client ${this.id} disconnected :(`);
-
+ 
 	// let everyone connected know that player has disconnected
 	this.broadcast.emit('player:disconnected', players[this.id]);
-
+ 
 	// remove user from list of connected players
 	delete players[this.id];
 };
-
+ 
 // Check if two players are online
-
+ 
 // Match 1 vs 1 players
-
+ 
 // Start game
 const startGame = () => {
 	// emit delay and random virus
 };
-
+ 
 // Handle when virus is clicked
 const handleClick = function () {
 	debug('Someone clicked on the virus');
-
+ 
 	// Gets random virus position and delay on each click
-
+ 
 	// this.emit('virus:position', getVirus);
 	setTimeout(() => {
 		this.emit('virus:position', getRandomPosition(), getRandomPosition());
 	}, getRandomDelay());
 };
 
-// // Compare reaction time and update score
-// const handleReactionTime = function (reactionTime) {
-// 	const player = 
-// };
+// Compare reaction time and update score //! inte klar
+const handleReactionTime = function (reactionTime) {
+	// get current room id
+	const roomId = roomId(this.id, rooms, thisGame);
 
-// Decide winner
+	// save reaction time (playerId from Model)
+	const player = playerId(this.id, roomId, thisGame);
+	player.elapsedTime = reactionTime;
 
-// Start new game.
-// 1. Display virus
-// 2. Update score
-// 3.
+	//emit
+};
+ 
 
-// Update scoreboard.
-
+ 
 module.exports = function (socket, _io) {
 	io = _io;
 
@@ -105,5 +103,5 @@ module.exports = function (socket, _io) {
 	socket.on('virus:click', handleClick);
 
 	// handle reaction time
-	//socket.on('reaction-time', handleReactionTime);
+	socket.on('reaction-time', handleReactionTime);
 };
