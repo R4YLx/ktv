18883e5a0b35a74570a03f9b8aa23daa4494a46d
50773sa <93046590+50773sa@users.noreply.github.com>
2022-03-22 12:03:04 +0100

    Renamed models and moved it to controllers-map

---
+++ controllers/functions.js
@@ -0,0 +1,51 @@
+/*/////
+//  Modules
+/////*/
+
+
+//get room id
+const roomId = (rooms, id) => {
+    return rooms.find(room => room.id === id);
+};
+
+
+// get player by room id
+const playerId = (id, rooms, roomId) => {
+    return rooms[roomId].players.find (player => player.id !== id);
+};
+
+
+// get opponent id
+const opponentId = (id, rooms, roomId) => {
+    return rooms[roomId].players.find (player => player.id == id);
+};
+
+
+// check reaction time and give points
+const score = (player, opponent) => {
+    if (player.elapsedTime < opponent.elapsedTime) {
+        player.score++;
+
+    }   else {
+        opponent.score++;
+    }
+};
+
+
+// get result (winner)
+const winner = (player, opponent) => {
+    if (player.score < opponent.score) {
+        return opponent;
+    }   else {
+        return player; //! hur visa för vinnaren. hur ska det se ut?
+    }
+};
+
+
+module.exports = {
+    roomId,
+    playerId,
+    opponentId,
+    score,
+    winner,
+};
