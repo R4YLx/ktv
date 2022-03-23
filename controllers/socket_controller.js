5b6b95ab7643336e5b4bcfc29204bca8549ec413
Raymond Lam <raymond.m.lam@gmail.com>
2022-03-23 19:57:49 +0100

    restructered html and css

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -3,17 +3,7 @@
  */
 
 const debug = require('debug')('ktv:socket_controller');
-let io = null; //sätter io till null för att kunna lägga in player-objects här i sen när de connectat?
-
-let rooms = 1; //antal rum från början. när mer än 2 spelare vill spela ska de sen hamna i rum nr 2 osv.
-let gamesArray = []; //sätter en tom array för att sedan fylla med spel, där info om spelare och vilket rum de är i ska ligga. (vi fyller på med mer info sen)
-
-let players = {}; //players är ett tomt objekt. vi lägger spelare här i, de som är i samma rum ligger i samma players-objekt
-
-let count = 0;
-let rounds = 0;
-const maxRounds = 10;
-
+let io = null;
 /**
  *
  * Functions
@@ -31,150 +21,16 @@ const virusData = () => {
 	});
 };
 
-const getRoomById = id => {
-	return gamesArray.find(room => room.id === id);
-};
-
 /**
  *
  * Handling events
  *
  */
 
-// Handle connecting players
-const handlePlayerJoined = function (username, callback) {
-	players[this.id] = username; //sets the players id to be equal to their username instead. Utan denna rad så connectar de bara till ett rum och if-satsen på rad 74 körs ej
-
-	this.join(rooms);
-
-	if (Object.keys(players).length === 2) {
-		callback({
-			success: true,
-			//usersThatareOnline: Object.values(players), //hämtar ut innehållet i objektet players från usersthatareonline. men det behöver vi inte veta här
-		});
-
-		const room = rooms;
-
-		const room_id = this.id;
-
-		// add the room players are in and which 2 players that are in the room, to the games array
-		let thisGame = {
-			id: room_id,
-			room,
-			players,
-		};
-
-		debug(thisGame.players, room);
-
-		//pushes thisGame into the from start empty Games-array
-		gamesArray.push(thisGame);
-
-		debug('ROOOOM ID', gamesArray[0].id);
-
-		//io.emit('start game');
-		io.in(room).emit('game:start', players, virusData());
-
-		// empty players
-		players = {};
-
-		// when 2 people are in a room, a new room has to be crfeated for a 3rd player to join, and so on.
-		rooms++;
-	}
-};
-
-// Handle when virus is clicked
-const handleVirusClick = function (playerData) {
-	debug('Someone clicked on the virus');
-
-	debug('HAJSAN NY GREJ', gamesArray);
-
-	debug('Player 1 aka first player to join clicked info', playerData);
-
-	// let playersInGame = gamesArray.filter(x => x.id === this.id);
-
-	const gamework = gamesArray.filter(x => {
-		return (x = this.id);
-	});
-
-	debug('help us lord', gamework);
-
-	// debug('synTAX ERROR????', playersInGame);
-	count++;
-	let winner;
-
-	if (count % 2 !== 0) {
-		// give points to the fastest player (the first one that clicked is the first one in to the server)
-		playersInGame[playerData.id];
-		rounds++;
-
-		debug('how many playersInGame are here?', playersInGame);
-
-		debug('giving point if this works. Otherwise broken.');
-	}
-	// } else {
-	// 	// const randomVirus = virusData();
-
-	// 	if (rounds < maxRounds) {
-	// 		debug('random virus should appear if  this works');
-
-	// 		debug('length should be 2:', Object.keys(playersInGame).length);
-	// 		// emit new game round
-	// 		io.emit('game:newRound', playersInGame, virusData());
-	// 		// } else if (rounds === maxRounds) {
-	// 		// 	// check who the winner is
-	// 		// 	Object.values(players).map(player => {
-	// 		// 		if (player.score > 5) {
-	// 		// 			winner = player.name;
-	// 		// 			return winner;
-	// 		// 		}
-	// 		// 	});
-	// 		// 	// emit 'end-game'-event when 10 rounds has been played and let the clients know who is the winner
-	// 		// 	io.emit('end-game', players, winner);
-	// 		// 	delete players[this.id];
-	// 		// 	// reset game
-	// 		// 	rounds = 0;
-	// 		// 	players = {};
-	// 	}
-	// }
-};
-
-// Handle disconnecting players
-const handleDisconnect = function () {
-	debug(`Client with socket id ${this.id} disconnected :(`);
-
-	// let everyone connected know that player has disconnected
-	this.broadcast.emit('player:disconnected', players[this.id]);
-
-	// remove user from list of connected players
-	delete players[this.id];
-};
-
-// Compare reaction time and update score //! inte klar
-const handleReactionTime = function (reactionTime) {
-	// get current room id
-	const roomId = roomId(this.id, rooms, thisGame);
-
-	// save reaction time (playerId from Model)
-	const player = playerId(this.id, roomId, thisGame);
-	player.elapsedTime = reactionTime;
-
-	//emit
-};
+const handleConnect = function (username) {};
 
 module.exports = function (socket, _io) {
 	io = _io;
 
 	debug('On start of app: a new client has connected', socket.id);
-
-	// handle player connect
-	socket.on('player:join', handlePlayerJoined);
-
-	// handle click on virus
-	socket.on('virus:clicked', handleVirusClick);
-
-	// handle player disconnect
-	socket.on('disconnect', handleDisconnect);
-
-	// handle reaction time
-	socket.on('reaction-time', handleReactionTime);
 };
--- public/assets/js/game.js
+++ public/assets/js/game.js
@@ -1,18 +1,15 @@
 /*//////
 //  DOM Elements
 /////*/
-const currentRoundEl = document.querySelector('#currentRound');
-const exitGameButtonEl = document.querySelector('#exitGameButton');
-const gameAreaEl = document.querySelector('#gameArea');
+
 const gameWrapperEl = document.querySelector('#gameWrapper');
-const messageEl = document.querySelector('#message');
-const noticeEl = document.querySelector('#notice');
-const playersTimer = document.querySelector('#playersTimer');
-const playAgainButtonEl = document.querySelector('#playAgainButton');
-const showRoundsEl = document.querySelector('#showRounds');
+
 const startEl = document.querySelector('#start');
+
 const usernameFormEl = document.querySelector('#usernameForm');
+
 const virusEl = document.querySelector('#virus');
+
 const waitingEl = document.querySelector('#waiting');
 
 /*//////
@@ -21,15 +18,6 @@ const waitingEl = document.querySelector('#waiting');
 
 const socket = io();
 let username = null;
-
-// Get time and calculate
-let interval;
-let startTime;
-let reactionTime;
-let stopTime;
-let playerTime;
-let sum = 0;
-let timeSum = [];
 
 /*//////
 //  Functions 
@@ -55,37 +43,6 @@ const setInnerHTML = (element, value) => {
 	element.innerHTML = value;
 };
 
-// shows and hides notice element
-const showLightbox = () => {
-	if (noticeEl.style.display === 'grid') {
-		noticeEl.style.display = 'none';
-		hideElement(noticeEl);
-	} else {
-		noticeEl.style.display = 'grid';
-		displayElement(noticeEl);
-	}
-};
-
-// const timer = element => {
-// 	document.querySelector(element).innerHTML = (reactionTime / 1000).toFixed(3); //(3)- is nr of decimals
-// };
-
-//! OK. rör ej
-// start timer when virus is on display
-// let startTimer = () => {
-// 	let startTime = Date.now();
-// 	interval = setInterval(function () {
-// 		reactionTime = Date.now() - startTime;
-// 		timer('#playerOneTime', reactionTime);
-// 	}, 100);
-// };
-
-// Stop timer
-// let stopTimer = () => {
-// 	stopTime = Date.now();
-// 	clearInterval(interval);
-// };
-
 const getRandomVirus = virusData => {
 	virusEl.style.gridColumn = `${virusData.col} / span 1`;
 	virusEl.style.gridRow = `${virusData.row} / span 1`;
@@ -95,52 +52,6 @@ const getRandomVirus = virusData => {
 	}, virusData.delay);
 };
 
-//Display "waiting for other players"
-const displayWaitingForPlayers = () => {
-	hideElement(startEl); //släcker register-rutan
-	displayElement(waitingEl); //visar "waiting for another player-ruta"
-};
-
-const startGame = (players, virusData) => {
-	document.querySelector('#players').innerHTML = Object.values(players)
-		.map(
-			player =>
-				`<h3 class="playerScore">${player.name}<span>:</span><span class="score">${player.score}</span><span>:</span><span class="time">${player.time}</span></h3>`
-		)
-		.join(' ');
-
-	hideElement(waitingEl);
-	displayElement(gameWrapperEl);
-	getRandomVirus(virusData);
-};
-
-const newRound = virusData => {
-	virusEl.style.display = 'none';
-	// displayElement(virusEl);
-	console.log('random virus should appear within 5 sec');
-
-	if (Object.keys(players).length === 2) {
-		getRandomVirus(virusData);
-		updateScoreBoard(players);
-		console.log('this text is visible if this if-statment works');
-	}
-};
-
-// Update scoreboard. Get score from server
-const updateScoreBoard = players => {
-	// showRoundsEl.innerText = `${players[0].rounds}/10`;
-	// players.forEach(player => {
-	// 	playersTimer.innerHTML += `<p>${player.reactionTime}</p>`;
-	// });
-
-	document.querySelector('#players').innerHTML = Object.values(players)
-		.map(
-			player =>
-				`<h3 class="playerScore">${player.name}<span>:</span><span class="score">${player.score}</span><span>:</span><span class="time">${player.time}</span></h3>`
-		)
-		.join(' vs ');
-};
-
 /*//////
 //  Events
 /////*/
@@ -148,59 +59,11 @@ const updateScoreBoard = players => {
 // Register new player
 usernameFormEl.addEventListener('submit', e => {
 	e.preventDefault();
-
-	username = usernameFormEl.username.value;
-	displayWaitingForPlayers();
-
-	socket.emit('player:join', username, status => {
-		if (status.success) {
-			startGame();
-		}
-	});
 });
 
 // Click event for virus
-virusEl.addEventListener('click', () => {
-	stopTime = Date.now();
-	reactionTime = (stopTime - startTime) / 1000;
-	hideElement(virusEl);
-
-	playerData = {
-		time: reactionTime,
-		id: socket.id,
-	};
-	console.log(playerData);
-
-	socket.emit('virus:clicked', playerData);
-});
-
-// Play again when game over
-playAgainButtonEl.addEventListener('click', e => {
-	if (e.target.getAttribute('type') === 'playAgainButton') {
-		score = 0;
-		setInnerText(playerScoreEl, score);
-		setInnerText(currentRoundEl, score);
-		currentRoundEl.innerHTML = 0;
-		messageEl.innerHTML = ''; //nollställer "Grattis-meddelandet"
-	}
-	//Exit game-event
-	if (e.target.getAttribute('type') === 'exitGameButton') {
-		hideElement(gameWrapperEl);
-		messageEl.innerHTML = `
-		<p>YOU LOST!, ${username}</p> 
-		`;
-		//messageEl.innerHTML = ''; //nollställer "Grattis-meddelandet"
-	}
-});
-
-// Restart game on disconnect
+virusEl.addEventListener('click', () => {});
 
 /*//////
 //  Socket events
 /////*/
-
-socket.on('game:start', startGame);
-
-socket.on('game:newRound', newRound);
-
-socket.on('player:updateScore', updateScoreBoard);
