6a16c95d9ea928dca94f84a32185496e561673e1
jo-oo <johannakoreo@gmail.com>
2022-03-24 14:25:55 +0100

    added debugging

--- controllers/socket_controller.js
+++ controllers/socket_controller.js
@@ -31,13 +31,17 @@ handleConnect = function (username) {
 		score: 0,
 		reactionTime: null,
 	};
+	debug("This i a player object of connected player outside of the queue" + this.playerData.id);
 
 	// find another player
-	if (playQueue.length) {
-		const playerOne = this;
-		const playerTwo = playQueue.pop();
+	if (playQueue.length) { //om det finns en array med 1 st spealre i som väntar:
+	debug("this is play Queue" + playQueue.length) //visar 1.= hur många som är i playQueue
+		const playerOne = this; //hämtar ut this player objectet på den enda som är i kön
+		const playerTwo = playQueue.pop(); //sätter player 2 till att vara id på spelare 1
+		debug('playerOne', playerOne.id);
+		debug('playerTwo', playerTwo.id);
 
-		const roomId = `${playerOne.id}#${playerTwo.id}`;
+		const roomId = `${playerOne.id}`;
 
 		debug('playerOne', playerOne.id);
 		debug('playerTwo', playerTwo.id);
@@ -46,17 +50,20 @@ handleConnect = function (username) {
 		playerOne.join(roomId);
 		playerTwo.join(roomId);
 
+		debug('playerOne', playerOne.id);
+		debug('playerTwo', playerTwo.id);
+
 		activeGames[roomId] = {
 			players: [{ ...playerOne.playerData }, { ...playerTwo.playerData }],
 			gameRound: 1,
 		};
 
-		// start new game
+
+		// start new game -skickar detta till funktionen startGame i game.js och tar in id och erstter id och oponnent med player1 och player2.
 		startGame(playerOne, playerTwo, roomId);
 		return;
 	}
-
-	playQueue.push(this); //this = spelaren
+		playQueue.push(this); //this = spelaren
 
 	this.emit('player:waiting'); //
 };
@@ -86,3 +93,4 @@ module.exports = function (socket, _io) {
 
 	socket.on('player:connected', handleConnect);
 };
+ 
